84eee19ce601224292aebe81f30ad521fa1c4e67
kushallikhi <kushal@codalien.com>
2018-04-17 10:20:32 +0530

    updated logs path

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 
 kill $(ps aux | grep system_monitor_gg | awk '{print $2}') || true
 
-STDOUT_PATH="~/sys_monit_graylog.out"
+STDOUT_PATH="$HOME/sys_monit_graylog.out"
 nohup node start system_monitor_gg  >> $STDOUT_PATH 2>&1&
 
 echo "Daemon logs going to: $STDOUT_PATH"
